6bbc899b43232817553a8a93a19875a6dd72636b
LeeHanJo <gps95wwkhhev@gmail.com>
2024-04-11 18:49:25 +0900

    -first commit-

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminList from '../views/AdminList.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/adminlist',
+    name: 'adminlist',
+    component: AdminList
   },
   {
     path: '/about',
---
+++ src/views/AdminList.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="adminlist">
+   <h1>관리자 페이지 입니다.</h1>
+  </div>
+</template>
