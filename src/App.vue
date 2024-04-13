585acc1dc6f15286ccefa5e029ae3d2fcc9ee4db
PARKCY\Park <mangoperry2015@gmail.com>
2024-04-14 01:47:05 +0900

    bootstrap 패키지 추가(package.json 의존성 버전 수정)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">정책정보</router-link> |
+    <router-link to="/about">지원추천</router-link> |
+    <router-link to="/about">커뮤니티</router-link>
   </nav>
   <router-view/>
 </template>
