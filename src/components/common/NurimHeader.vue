32108b52806f4b7bf5c73cea5c8bfb10cf70359a
ChaeYeon Park <mangoperry2015@gmail.com>
2024-04-18 20:03:00 +0900

    [feat] header, footer 수정

--- src/components/common/NurimHeader.vue
+++ src/components/common/NurimHeader.vue
@@ -8,16 +8,15 @@
       <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <!-- identity -->
         <div class="col-md-3 mb-2 mb-md-0">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <router-link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <span class="fs-4">Nurim+ BI</span>
-          </a>
+          </router-link>
         </div>
 
         <!-- menu link -->
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-          <li><router-link to="#" class="nav-link px-2 link-secondary">Home</router-link></li>
           <li><router-link to="#" class="nav-link px-2">정책정보</router-link></li>
-          <li><router-link to="#" class="nav-link px-2">지원금 추천</router-link></li>
+          <li><router-link to="#" class="nav-link px-2">나를 위한 정책지원</router-link></li>
           <li><router-link to="#" class="nav-link px-2">커뮤니티</router-link></li>
           <!-- <li><a href="#" class="nav-link px-2">About</a></li> -->
         </ul>
