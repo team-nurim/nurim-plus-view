41b2d2494d7117d02bfad5bcdf7dac98f3b59bd5
jh2 <wednestues18@gmail.com>
2024-04-21 12:10:42 +0900

    middle commit

--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -1,11 +1,21 @@
- <!-- eslint-disable  -->
 <template>
-    <div class="message" :class="{'owner': isOwner}">
-      <p>{{ message.text }}</p>
-    </div>
+  <div class="message" :class="{'owner': isOwner}">
+    <p>{{ message.text }}</p> <!-- 메시지의 텍스트를 표시 -->
+  </div>
 </template>
 
-  <script>
+<script>
+export default {
+  props: {
+    message: Object, // 메시지 객체를 전달받습니다.
+    isOwner: Boolean // 메시지 소유자 여부를 판단합니다.
+  }
+};
+</script>
+
+
+
+  <!-- <script>
   export default {
     props: {
       message: Object,
@@ -13,7 +23,7 @@
       isOwner: Boolean
     }
   };
-  </script>
+  </script> -->
 
   <style scoped>
   .message {
@@ -27,3 +37,7 @@
     align-self: flex-end;
   }
   </style>
+ <!-- 이 파일해석: fetchsupportfunds메소드로
+  api데이터를 가져와 이를 메시지 배열에 저장하여
+  MessageComponent를 사용하여 화면에 표시함.
+  이걸 컴포넌트로 만들어 ,vue프로젝트에 포함시켜 사용하면됨 -->
