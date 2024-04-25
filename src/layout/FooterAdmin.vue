f7451a861d44157c19d82db8099cfc076488dd2f
ChaeYeon Park <mangoperry2015@gmail.com>
2024-04-25 17:25:52 +0900

    [fix] admin 디자인 수정

--- src/layout/FooterAdmin.vue
+++ src/layout/FooterAdmin.vue
@@ -1,50 +1,33 @@
 <template>
-  <footer class="mt-5 py-5">
-    <div class="container text-center">
-      <div class="row" style="color: white;">
-        <div class="col-md-4">
-          <h5>About Us</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  <!-- // eslint-disable-next-line
+  /* eslint-disable */ -->
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+      <div class="container mb-2">
+        <div class="copyright" style="margin:1.2rem; font-size:0.8rem; text-align:left; color:#999">
+          &copy; Copyright <strong><span>(주)누림</span></strong>. All Rights Reserved<br>
+          <span>대표자 : 박채연 | 개인정보보호책임자 : 백승용</span><br>
+          <span>사업자등록번호 : 150-86-12345</span><br>
+          <span>통신판매업신고번호 : 2024-서울강남-01234</span><br>
+          <span>사업장소재지 : 서울특별시 강남구 강남대로 405 8층</span><br>
+          <span>대표번호 : 1588-1111 | 이메일 : support@nurim.co.kr</span><br>
+          <span>운영시간 : (평일) 09:30-18:10 (점심) 13:20-14:20 | 주말∙공휴일 휴무</span><br>
         </div>
-        <div class="col-md-4">
-          <h5>Contact</h5>
-          <ul class="list-unstyled">
-            <li>Address: 1234 Main St, City, Country</li>
-            <li>Phone: +1234567890</li>
-            <li>Email: info@example.com</li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <h5>Follow Us</h5>
-          <ul class="list-inline">
-            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/ -->
         </div>
       </div>
-      <hr>
-      <p style="color: white;">&copy; 2024 Your Company. All rights reserved.</p>
-    </div>
-  </footer>
-</template>
-
-<script>
-export default {
-  name: 'FooterAdmin'
-}
-</script>
-<style>
-    footer {
-        background: #444444; /* 그라데이션 배경 */
-        padding: 30px 0;
-          color: white; /* Text color */
-    }
-
-    footer a {
-        color: #ffffff; /* 링크 색상 */
-    }
-    .h5{
-      color: white;
-    }
-</style>
+    </footer>   <!-- End Footer -->
+  </template>
+  
+  <style scoped>
+  .footer {
+    padding: 2rem 0rem;
+    bottom: 0;
+    background-color: #F5F5F5;
+  }
+  </style>
+  
