4ace4e676b8482d5e30483b4e4beca1d6ce9695e
ChaeYeon Park <mangoperry2015@gmail.com>
2024-04-29 10:47:53 +0900

    [fix] 커뮤니티 등록하기 화면 수정

--- src/views/community/CommunityCreateView.vue
+++ src/views/community/CommunityCreateView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="register-container">
+<main class="mt-5 mb-5">
+  <div class="container" style="max-width: 800px;">
+
     <!-- 제목, 카테고리 입력 필드 -->
-    <div class="form-group">
-      <div class="col-md-1">
-        <select id="categorySelect" class="form-select" name="category" style="margin-left: 175px;" v-model="category">
+    <div class="form-group mb-3">
+      <div class="col-md-3 mb-3">
+        <select id="categorySelect" class="form-select" name="category" v-model="category">
           <option disabled value="">카테고리</option>
           <option value="보육">보육</option>
           <option value="출산">출산</option>
@@ -11,47 +13,56 @@
           <option value="장례">장례</option>
         </select>
       </div>
-      <label for="titleInput">제목</label>
-      <input type="text" class="form-control" id="titleInput" v-model="title" placeholder="제목을 입력하세요" style="width: 1000px; margin-left: auto; margin-right: auto;">
-      <span v-if="!title" style="color: red; margin-left: 10px;">제목을 입력해주세요.</span>
+
+      <div class="col text-start">
+        <label for="titleInput">제목</label>
+        <input type="text" class="form-control" id="titleInput" v-model="title" placeholder="제목을 입력하세요">
+        <span v-if="!title" style="color: red;">제목을 입력해주세요.</span>
+      </div>
     </div>
 
     <!-- 내용 입력 필드 -->
-    <div class="form-group">
+    <div class="form-group mb-3 text-start">
       <label for="contentInput">내용</label>
-      <textarea class="form-control" id="contentInput" v-model="content" rows="5" placeholder="내용을 입력하세요" style="width: 1000px; height: 250px; margin-left: auto; margin-right: auto;"></textarea>
-      <span v-if="!content" style="color: red; margin-left: 10px;">내용을 입력해주세요.</span>
+      <div class="col w-100">
+        <textarea class="form-control" id="contentInput" v-model="content" rows="5" style="max-width:100%;" placeholder="내용을 입력하세요"></textarea>
+        <span v-if="!content" style="color:red;">내용을 입력해주세요.</span>
+      </div>
     </div>
+
     <!-- 이미지 업로드를 위한 input 요소 추가 -->
-    <label for="image" class="col-md-3 col-form-label">이미지 업로드</label>
-    <div class="mb-3 row justify-content-center">
-      <div class="col-md-9">
-        <input type="file" class="form-control" id="image" accept="image/*" @change="handleImageUpload" multiple>
+    <div class="mb-3 text-start">
+      <label for="image" class="col-md-3 col-form-label">이미지 업로드</label>
+      <div class="row justify-content-center mb-3">
+        <div class="col">
+          <input type="file" class="form-control" id="image" style="width:100%;" accept="image/*" @change="handleImageUpload" multiple>
+        </div>
       </div>
     </div>
+
     <!-- 이미지 미리보기 -->
-<div v-if="previewImages.length > 0" class="row">
-  <div class="col-md-8 offset-md-2">
-    <h3 class="text-center mb-3">미리보기</h3>
-    <div class="image-list d-flex flex-wrap justify-content-start"> <!-- 이미지를 가로로 배열하기 위해 flex 속성을 이용합니다 -->
-      <div v-for="(previewImage, index) in previewImages" :key="index" class="image-item mx-2 my-2" style="max-width: 200px;"> <!-- 각 이미지와 삭제 버튼을 감싸는 div 요소에 margin을 조정하여 간격을 조절합니다 -->
-        <img :src="previewImage" alt="이미지" class="img-fluid mr-2" loading="lazy" style="max-width: 150px; max-height: 150px;"> <!-- 이미지 사이에 간격을 조절합니다 -->
-        <button class="btn btn-sm delete-image-btn" @click="deleteImage(index)"><i class="fa-solid fa-trash"></i></button>
+    <div v-if="previewImages.length > 0" class="row">
+      <div class="col offset-md-2">
+        <h3 class="text-center mb-3">미리보기</h3>
+        <div class="image-list d-flex flex-wrap justify-content-start"> <!-- 이미지를 가로로 배열하기 위해 flex 속성을 이용합니다 -->
+          <div v-for="(previewImage, index) in previewImages" :key="index" class="image-item mx-2 my-2"> <!-- 각 이미지와 삭제 버튼을 감싸는 div 요소에 margin을 조정하여 간격을 조절합니다 -->
+            <img :src="previewImage" alt="이미지" class="img-fluid mr-2" loading="lazy"> <!-- 이미지 사이에 간격을 조절합니다 -->
+            <button class="btn btn-sm delete-image-btn" @click="deleteImage(index)"><i class="fa-solid fa-trash"></i></button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
     <!-- 취소하기 및 등록하기 버튼 -->
-    <div class="row" style="margin-top: 20px;">
-      <div class="col">
-        <button class="btn btn-secondary" @click="cancel">취소하기</button>
-      </div>
-      <div class="col">
+    <div class="row mt-5 mb-5">
+      <div class="col text-end">
+        <button class="btn btn-secondary me-2" @click="cancel">취소하기</button>
         <button class="btn btn-primary" @click="createBoard" :disabled="category === '' || !title || !content">등록하기</button>
       </div>
     </div>
+
   </div>
+</main>
 </template>
 
 <script>
@@ -156,16 +167,5 @@ export default {
 }
 </script>
 
-<style>
-.register-container {
-  width: 1350px;
-  height: 100vh;
-  margin-left: auto;
-  margin-right: auto;
-}
-.form-control {
-  width: 300px;
-  height: 50px;
-  border: 2px solid black;
-}
+<style scoped>
 </style>
